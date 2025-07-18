396ce322351abd49c2f0e2da621e045cb5f39ea1
SahithyaKadiyamsetti <sahithyaKadiyamsetti@gmail.com>
2025-07-18 11:25:47 +0530

    Added exception handling and updated logging

--- src/main/java/com/revature/day05/Account.java
+++ src/main/java/com/revature/day05/Account.java
@@ -1,4 +1,30 @@
 package com.revature.day05;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Account {
+    private double balance;
+
+    private static final Logger logger= LoggerFactory.getLogger(Account.class);
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance=balance;
+
+    }
+
+        public double deposit(double amount){
+           logger.info("inside the deposit method");
+            System.out.println("inside deposit method");
+            balance+=amount;
+        return (this.getBalance()+amount);
+    }
+    public double withdraw(double amount){
+        logger.warn("inside the withdraw method");
+        System.out.println("inside withdraw method");
+        balance-=amount;
+        return (this.getBalance()-amount);
+
+    }
 }
--- src/main/java/com/revature/day05/AccountMain.java
+++ src/main/java/com/revature/day05/AccountMain.java
@@ -1,4 +1,12 @@
 package com.revature.day05;
 
 public class AccountMain {
+    public static void main(String[] args) {
+        Account account=new Account();
+        account.setBalance(1000);
+        System.out.println("balance after deposit : " + +account.deposit(300));
+        System.out.println("balance after withdraw : " + +account.withdraw(200));
+
+
+    }
 }
